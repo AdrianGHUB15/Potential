c9a72ab0b4398b7346de6e5f0a371cb555e6ae42
Adrian Ladoni <274005880+AdrianGHUB15@users.noreply.github.com>
2026-09-07 17:38:23 +0800

    only no data move

--- src/search.c
+++ src/search.c
@@ -240,7 +240,6 @@
     ║ Internal Iterative Reductions ║
     ╚═══════════════════════════════╝*/
   TUNE_INT IIR_DEPTH = 8;
-  TUNE_INT IIR_TT_DEPTH_SUBTRACTOR = 3;
 
   /*╔══════════════════════════════╗
     ║      Aspiration Windows      ║
@@ -1066,7 +1065,7 @@ int negamax(int alpha, int beta, int depth, ThreadData *t, my_time* time, Search
     improving = !in_check && (ss - 2)->staticEval != noEval && ss->staticEval > (ss - 2)->staticEval;
 
     // Internal Iterative Reductions
-    if ((pvNode || predicted_cut_node) && depth >= IIR_DEPTH && (!tt_move || tt_depth < depth - IIR_TT_DEPTH_SUBTRACTOR)) {
+    if ((pvNode || predicted_cut_node) && depth >= IIR_DEPTH && tt_move) {
         depth--;
     }
 
